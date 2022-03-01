dd7f1316a67142ef0c24c8e5e4bdc78e509ae245
reinautomata <michelangelobasso.23@gmail.com>
2022-03-01 15:10:05 +0100

    add ruby methods

--- hello.rb
+++ hello.rb
@@ -1,8 +1,6 @@
-# Lesson 1: Outputting a String
+puts "What is your favorite color?"
+color = gets.chop
 
-puts "What's your name?"
-
-# Lesson 2: Prompting User Input
-
-name = gets.chop
-puts "Hey, #{name}! Nice to meet you!"
+if color == "blue"
+    puts "Wow! My favorite color is #{color} also!"
+end
