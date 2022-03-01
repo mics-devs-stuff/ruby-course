be4e6b8b826a76cb314b576fc3158b77d5bcf66e
reinautomata <michelangelobasso.23@gmail.com>
2022-03-01 15:13:42 +0100

    add else statement

--- hello.rb
+++ hello.rb
@@ -1,8 +1,8 @@
-puts "What is your favorite color?"
-color = gets.chop
+# Lesson 1: Outputting a String
 
-if color == "blue"
-    puts "Wow! My favorite color is #{color} also!"
-else
-    puts "I'm not a big fan of #{color}."
-end
+puts "What's your name?"
+
+# Lesson 2: Prompting User Input
+
+name = gets.chop
+puts "Hey, #{name}! Nice to meet you!"
